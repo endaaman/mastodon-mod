87128da814a7758f991ad002e569032463ac3548
endaaman <buhibuhidog@gmail.com>
2017-06-06 18:09:16 +0900

    oreore style

--- script.js
+++ script.js
@@ -1,15 +1,22 @@
-var config = document.createElement( "script" ) ;
-config.type = "text/x-mathjax-config" ;
-config.appendChild( document.createTextNode( "MathJax.Hub.Config({tex2jax: {inlineMath: [ ['$','$'], ['\\\\(', '\\\\)'] ] }});" ) ) ;
+var config = document.createElement('script')
+config.type = 'text/x-mathjax-config'
+config.appendChild(
+  document.createTextNode(
+    'MathJax.Hub.Config({tex2jax: {inlineMath: [ ['$','$'], ['\\\\(', '\\\\)'] ] }})'
+  )
+)
 
-document.head.appendChild(config) ;
+document.head.appendChild(config)
 
-var script = document.createElement( "script" ) ;
-script.type = "text/javascript" ;
-script.async="async" ;
-script.src="https://cdnjs.cloudflare.com/ajax/libs/mathjax/2.7.0/MathJax.js?config=TeX-AMS_SVG" ;
+var script = document.createElement('script')
+script.type = 'text/javascript'
+script.async = 'async'
+script.src = 'https://cdnjs.cloudflare.com/ajax/libs/mathjax/2.7.0/MathJax.js?config=TeX-AMS_SVG'
 
-script.appendChild( document.createTextNode('window.setInterval( function() { MathJax.Hub.Queue(["Typeset",MathJax.Hub]); }, 1000 ) ;') ) ;
+script.appendChild(
+  document.createTextNode(
+    'window.setInterval( function() { MathJax.Hub.Queue(["Typeset",MathJax.Hub]) }, 1000 ) '
+  )
+)
 
-document.head.appendChild(script) ;
-
+document.head.appendChild(script)
