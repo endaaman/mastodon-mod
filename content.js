1aea99455cdf312a93b10be99b173efdf040cf4d
endaaman <buhibuhidog@gmail.com>
2017-06-11 02:26:42 +0900

    fix coding style

--- content.js
+++ content.js
@@ -1,11 +1,11 @@
-chrome.runtime.onMessage.addListener(function (request) {
-  replaceSelectedText(document.activeElement, request.text);
-});
+chrome.runtime.onMessage.addListener(function(request) {
+  replaceSelectedText(document.activeElement, request.text)
+})
 
 function replaceSelectedText(elem, text) {
-  var start = elem.selectionStart;
-  var end = elem.selectionEnd;
-  elem.value = elem.value.slice(0, start) + text + elem.value.substr(end);
-  elem.selectionStart = start + text.length;
-  elem.selectionEnd = elem.selectionStart;
+  const start = elem.selectionStart
+  const end = elem.selectionEnd
+  elem.value = elem.value.slice(0, start) + text + elem.value.substr(end)
+  elem.selectionStart = start + text.length
+  elem.selectionEnd = elem.selectionStart
 }
